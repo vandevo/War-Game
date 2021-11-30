4c346a995ef24e6fb946eba026909d2593fdfb08
Van Vo <hello@vandevo.com>
2021-11-30 19:38:00 +0000

    master

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ function callBack(){
     }
 
     setTimeout(callBack, 20000)
+
+
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
+
+
+function gimmeThePets(person){
+    return person.hasPet
+}
+
+const peopleWithPets = people.filter(gimmeThePets)
+
+function peopleHasPets(person, callback){
+    
+    
+    const resultingArray = []
+    
+    for (let item of array){
+        const shouldBeIncluded = callback(item)
+        if (shouldBeIncluded){
+            resultingArray.push(item)
+        }
+    }
+        return resultingArray
+
+}
