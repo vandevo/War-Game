4e7061108c2d8593ea35d61c4713f988d2561372
Van Vo <hello@vandevo.com>
2021-11-30 17:03:46 +0000

    master

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const newDeck = document.getElementById("newDeck")
-function mynewDeck() {
+function handleClick() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then(response => response.json())
     .then(data => console.log(data));
 }
 
-newDeck.addEventListener("click", mynewDeck)
+newDeck.addEventListener("click", handleClick)
 
+function callBack(){
+    }
+
+    setTimeout(callBack, 20000)
